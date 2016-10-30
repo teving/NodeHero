94e804f120d1c705543388fa8342f4d5d255d236
Kevin Gurton <kevin.gurton@gmail.com>
2016-10-30 19:31:45 +0000

    Async file read

--- app/index.js
+++ app/index.js
@@ -1,3 +1,14 @@
-const _ = require('lodash')
+const fs = require('fs')
 
-_.assign({'a': 1}, {'b': 2}, {'c': 3});
+console.log('start reading a file...');
+
+fs.readFile('file.md', 'utf-8', function(err, content){
+	if (err){
+		console.log('error happened during reading the file');
+		return console.log(err)
+	}
+
+	console.log(content);
+});
+
+console.log('end of the file');
