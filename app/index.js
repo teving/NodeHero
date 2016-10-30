b54f77b6a331bbb097cb659d5d1bb7ec5419427e
Kevin Gurton <kevin.gurton@gmail.com>
2016-10-30 19:15:36 +0000

    Simple implementation of requesting numbers

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
 const calc = require('./calc')
+const readline = require('readline');
+
 
 const numbersToAdd = [
 	3,
@@ -9,3 +11,26 @@ const numbersToAdd = [
 
 const result = calc.sum(numbersToAdd)
 console.log(`The result is ${result}`)
+
+const interface = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+console.log('before first question');
+interface.question('Enter first number to add:', answer => {
+	let firstNumber, secondNumber;
+	firstNumber = +answer;
+console.log('before second question');
+	interface.question('Enter second number to add:', answer => {
+		secondNumber = +answer;
+
+		const result2 = firstNumber + secondNumber;
+		console.log(`The result is ${result2}`);
+
+		interface.close();
+	});
+});
+
+console.log('Enter second number to add:');
+
