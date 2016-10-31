693f1cf9e4cb34f10a2baad8856a3aca10eb522e
Kevin Gurton <kevin.gurton@gmail.com>
2016-10-31 13:36:16 +0000

    First express app

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-require('./app/index');
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const port = 3000;
+
+const app = express();
+
+app.engine('.hbs', exphbs({
+	defaultLayout: 'main',
+	extname: '.hbs',
+	layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (request, response) => {
+	response.render('home', {
+		name: 'Kevin'
+	});
+});
+
+app.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
