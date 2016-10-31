6b010041915e6b1664d78fca19913647e498ebd0
Kevin Gurton <kevin.gurton@gmail.com>
2016-10-31 13:02:19 +0000

    Using express middleware

--- app/index.js
+++ app/index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use((request, response, next) => {
+	console.log(request.headers);
+	next();
+});
+
+app.use((request, response, next) => {
+	request.chance = Math.random();
+	next();
+});
+
 app.get('/', (request, response) => {
-	response.send('Hello from express');
+	response.json({
+		chance: request.chance
+	});
 });
 
 app.listen(port, (err) => {
